fa429ae7f709223f7c03cb77742ea72919bfc8af
lishenfei <lishenfei@xunlei.com>
2017-01-13 17:44:21 +0800

    new: ApplicationStartupListener

--- src/main/java/com/sfl/video/ApplicationStartupListener.java
+++ src/main/java/com/sfl/video/ApplicationStartupListener.java
@@ -3,12 +3,10 @@ package com.sfl.video;
 import com.sfl.video.service.VideoService;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.stereotype.Component;
 
 /**
  * Created by lishenfei on 2017-01-13.
  */
-@Component
 public class ApplicationStartupListener implements ApplicationListener<ContextRefreshedEvent> {
 
     @Override
